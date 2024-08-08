2edac3105c1f71141703c642195557f2cd5f207b
julesFa <jules.fauchon@yahoo.fr>
2024-08-08 10:39:56 +0200

    Update entrypoint.sh destination is now build-root and not build-root/html

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,3 +16,6 @@ fi
 
 # TZ is because of bazel issue see https://github.com/nektos/act/issues/1853
 TZ=UTC .venv/bin/sphinx-build -M html $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT
+
+cp $BUILD_ROOT/html/* $BUILD_ROOT
+rm -rf $BUILD_ROOT/html
