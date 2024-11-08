f8f0776713df348aca6efe87ca7a5e53cd751e3c
jules.fauchon <jules.fauchon@hawai.tech>
2024-11-08 11:50:33 +0100

    Try to create directory per branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 
 SOURCE_ROOT=$1
 BUILD_ROOT=$2
+BRANCH_NAME=$3
 
-mkdir -p $BUILD_ROOT/$GITHUB_REPOSITORY
+mkdir -p $BUILD_ROOT/$GITHUB_REPOSITORY/$BRANCH_NAME
 
 python3 -m venv .venv
 
@@ -15,4 +16,4 @@ else
 fi
 
 # TZ is because of bazel issue see https://github.com/nektos/act/issues/1853
-TZ=UTC .venv/bin/sphinx-build $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT/$GITHUB_REPOSITORY
+TZ=UTC .venv/bin/sphinx-build $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT/$GITHUB_REPOSITORY/$BRANCH_NAME
