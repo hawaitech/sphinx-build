82a33f4e6b005332187e75bbcbb39b5aead6f361
Jean Simatic <jean.simatic@hawaitech.com>
2025-06-11 14:49:37 +0200

    🔥 Slim down entrypoint

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ FROM ubuntu:latest
 RUN apt-get update && apt-get install -y pandoc
 # Add UV to the container
 COPY --from=ghcr.io/astral-sh/uv:latest /uv /uvx /bin/
+RUN /bin/uv python install 3.12
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 COPY ext/ /ext/
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,15 @@ mkdir -p $DST_DIR
 export TZ=UTC  # TZ is because of bazel issue see https://github.com/nektos/act/issues/1853
 export PYTHONPATH="/ext:$SOURCE_ROOT/ext"
 
-echo "installing pandoc"
-apt-get update && apt-get install -y pandoc
-
 echo "Setup uv"
-uv venv -p 3.12 .venv
-uv pip install -U sphinx
+uv venv
 
 if [ -f "$SOURCE_ROOT/requirements.txt" ]; then
     echo "Installation of requirements"
     uv pip install -r $SOURCE_ROOT/requirements.txt
 else
-    echo "No installation requirements found"
+    echo "No installation requirements found: defaulting to sphinx"
+    uv pip install -U sphinx
 fi
 
 uv run sphinx-build $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT/$GITHUB_REPOSITORY/$BRANCH_NAME
