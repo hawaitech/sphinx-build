27f25d4c66b4d2f31ac84262b7c7176286752481
Jean Simatic <jean.simatic@hawaitech.com>
2025-06-23 09:22:20 +0200

    ✨ Handle the python projects case

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,13 @@ mkdir -p $DST_DIR
 export TZ=UTC  # TZ is because of bazel issue see https://github.com/nektos/act/issues/1853
 export PYTHONPATH="/ext:$SOURCE_ROOT/ext"
 
-echo "Setup uv"
-uv venv
-
 if [ -f "$SOURCE_ROOT/requirements.txt" ]; then
-    echo "Installation of requirements"
-    uv pip install -r $SOURCE_ROOT/requirements.txt
+    UV_WITH=--with-requirements=$SOURCE_ROOT/requirements.txt
 else
-    echo "No installation requirements found: defaulting to sphinx"
-    uv pip install -U sphinx
+    UV_WITH=--with sphinx
 fi
 
-uv run sphinx-build $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT/$GITHUB_REPOSITORY/$BRANCH_NAME
+UV_CMD=uv run $UV_WITH --no-project sphinx-build $GITHUB_WORKSPACE/$SOURCE_ROOT $BUILD_ROOT/$GITHUB_REPOSITORY/$BRANCH_NAME
+
+echo $UV_CMD
+$UV_CMD
